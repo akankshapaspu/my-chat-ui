939fd18e1f8140cfbe21dd36db284dfa08c3bc65
akankshapaspu <101693062+akankshapaspu@users.noreply.github.com>
2025-04-28 00:45:47 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,16 @@
 // pages/_app.js
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
-import axios from "axios";
-
-// ⚠️ tell axios to always send cookies
-axios.defaults.withCredentials = true;
 
 export default function App({ Component, pageProps }) {
   return (
-    <SessionProvider session={pageProps.session}>
+    <SessionProvider
+      session={pageProps.session}
+      // optional: keep session fresh
+      refetchInterval={5 * 60}
+    >
       <Component {...pageProps} />
     </SessionProvider>
   );
 }
+
