35ae1681d4f78d49a51589787d4cd5939235abd5
akankshapaspu <101693062+akankshapaspu@users.noreply.github.com>
2025-04-28 00:40:18 -0700

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,31 +1,33 @@
-// pages/api/auth/[...nextauth].js
 import NextAuth from "next-auth";
 import EmailProvider from "next-auth/providers/email";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "../../../lib/mongodb";
 
-export const authOptions = {
+export default NextAuth({
   adapter: MongoDBAdapter(clientPromise),
 
   providers: [
     EmailProvider({
-      server: process.env.EMAIL_SERVER, // e.g. smtp://youremail@gmail.com:app_password@smtp.gmail.com:587
-      from: process.env.EMAIL_FROM,      // same email you put in the URL above
+      // instead of `server: process.env.EMAIL_SERVER`, pass an object:
+      server: {
+        host: process.env.EMAIL_HOST,
+        port: Number(process.env.EMAIL_PORT),
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
     }),
   ],
 
   secret: process.env.NEXTAUTH_SECRET,
 
-  session: {
-    strategy: "jwt",
-  },
-
+  session: { strategy: "jwt" },
   callbacks: {
     async session({ session, token }) {
       session.user.id = token.sub;
       return session;
     },
   },
-};
-
-export default NextAuth(authOptions);
+});
